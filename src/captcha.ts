eca01936f8375980d3a687aaddeb0a92cec012bf
Daniel Gong <daniel.gong@carboncredentials.com>
2017-11-18 19:22:48 +0000

    update

---
+++ src/captcha.ts
@@ -0,0 +1,51 @@
+/*
+import { Captcha } from './captcha';
+
+const captcha = new Captcha({
+	secret: '---'
+});
+
+captcha.verify('input_response', 'ip').then((response) => {
+	console.log(response);
+});
+*/
+
+import * as request from './request';
+
+export interface CaptchaConfig {
+	request?: typeof request;
+	secret: string;
+};
+
+export class Captcha {
+
+	static readonly RECAPTCHA_URL = 'https://www.google.com/recaptcha/api/siteverify';
+
+	protected _secret: string;
+	protected _request: typeof request;
+
+	/**
+	 * @param {object} options
+	 * @param {object} options.request - ./request
+	 * @param {object} options.secret - google captcha secret https://www.google.com/recaptcha/admin#site/337294176
+	 */
+    constructor (options: CaptchaConfig) {
+        this._request = options.request || request;
+        this._secret = options.secret;
+    }
+
+    verify (response: string, remoteip: string): Promise<{}> {
+        return this._request.post(
+            Captcha.RECAPTCHA_URL,
+            {
+                data: {
+                    secret: this._secret,
+                    response,
+                    remoteip
+                }
+            }
+        ).then(
+			(response) => response.json
+		);
+    }
+}
--- src/request.ts
+++ src/request.ts
@@ -1,11 +1,9 @@
-'use strict';
-
 /*
 example:
 
-let request = require('request.js');
+import {send, get, post} from './request';
 
-request.get('https://httpbin.org/get?a=b').then((respose) => {
+get('https://httpbin.org/get?a=b').then((respose) => {
     console.dir(respose.statusCode, {colors:true});
     console.dir(respose.headers, {colors:true});
     console.dir(respose.data, {colors:true});
@@ -13,23 +11,31 @@ request.get('https://httpbin.org/get?a=b').then((respose) => {
     console.dir(respose.json, {colors:true});
 });
 
-request.post('https://httpbin.org/post?a=b').then((respose) => {
+post('https://httpbin.org/post?a=b').then((respose) => {
     console.dir(respose.statusCode, {colors:true});
     console.dir(respose.headers, {colors:true});
     console.dir(respose.data, {colors:true});
     // if respose.data is a json string, respose.json will return the json object
     console.dir(respose.json, {colors:true});
 });
-
 */
 
 import * as url from 'url';
+import { stringify } from 'querystring';
 
 export interface RequestConfig {
     data?: any;
     headers?: {};
     method?: string;
+	protocol?: string;
 };
+
+export interface RequestResponse {
+	statusCode: number;
+	headers: {}[];
+	data: string;
+	json: {};
+}
 
 /**
  * @param {object} options
@@ -37,23 +43,24 @@ export interface RequestConfig {
  * @param {object} options.headers - http headers
  * @param {string} options.method - POST GET etc
  */
-export function send (urlString: string, options = {}) {
+export function send (urlString: string, options: RequestConfig = {}): Promise<RequestResponse> {
     return new Promise((resolve, reject) => {
         options = Object.assign(url.parse(urlString), options);
 
-        let request = require(options.protocol === 'http:' ? 'http' : 'https').request(options, (response) => {
+        const request = require(options.protocol === 'http:' ? 'http' : 'https').request(options, (response) => {
             response.setEncoding('utf8');
             let data = '';
             response.on('data', (chunk) => data += chunk);
             response.on('end', () => {
-                resolve({
+				const requestResponse: RequestResponse = {
                     statusCode: response.statusCode,
                     headers: response.headers,
                     data: data,
                     get json () {
                         return JSON.parse(data);
                     }
-                });
+                };
+                resolve(requestResponse);
             });
         });
 
@@ -69,28 +76,26 @@ export function send (urlString: string, options = {}) {
     });
 }
 
-Request = {
-    /**
-     * @param {object} options
-     * @param {object} options.headers - http headers
-     */
-    // get: (url, options = {}) => {
-        // delete options.data;
-        // return Request.send(url, options);
-    // },
+/**
+ * @param {object} options
+ * @param {object} options.headers - http headers
+ */
+export function get (url: string, options: RequestConfig = {}): Promise<RequestResponse> {
+	delete options.data;
+	return send(url, options);
+}
 
-    /**
-     * @param {object} options
-     * @param {object} options.data - post data e.g. form data or files
-     * @param {object} options.headers - http headers
-     */
-    // post: (url, options = {}) => {
-        // options.data = require('querystring').stringify(options.data || {});
-        // options.headers = Object.assign(options.headers || {}, {
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-            // 'Content-Length': Buffer.byteLength(options.data)
-        // });
-        // options.method = 'POST';
-        // return Request.send(url, options);
-    // }
-};
+/**
+ * @param {object} options
+ * @param {object} options.data - post data e.g. form data or files
+ * @param {object} options.headers - http headers
+ */
+export function post (url: string, options: RequestConfig = {}): Promise<RequestResponse> {
+	options.data = stringify(options.data || {});
+	options.headers = Object.assign(options.headers || {}, {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Content-Length': Buffer.byteLength(options.data)
+	});
+	options.method = 'POST';
+	return send(url, options);
+}
