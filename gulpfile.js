e089cd9b300eed2840c9f01594d7a4d17e2a25ad
Daniel Gong <daniel.gong@carboncredentials.com>
2017-12-02 11:03:42 +0000

    index updae

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,14 @@ const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 const changed = require('gulp-changed');
 const merge = require('merge2');
+const fs = require('fs');
+const path = require('path');
 
 const tsProject = ts.createProject('./tsconfig.json');
 
 const distFolder = 'dist';
 
-gulp.task('ts', () => {
+gulp.task('ts', ['index.ts'], () => {
     const tsResult = gulp.src('src/*.ts')
         .pipe(
             changed(distFolder, {extension: '.js'})
@@ -24,6 +26,22 @@ gulp.task('ts', () => {
         .pipe(sourcemaps.write()) // Now the sourcemaps are added to the .js file
         .pipe(gulp.dest(`${distFolder}/js`))
     ]);
+});
+
+gulp.task('index.ts', () => {
+    return new Promise((resolve) => {
+        const writeStream = fs.createWriteStream('src/index.ts');
+        fs.readdir('src', (error, files) => {
+            files.forEach((file) => {
+                const filename = path.basename(file).replace('.ts', '');
+                if (filename !== 'index' && filename !== 'test') {
+                    writeStream.write(`export * from './${filename}';\n`);
+                }
+            });
+            writeStream.end();
+            resolve();
+        });
+    });
 });
 
 gulp.task('watch', ['ts'], () => {
