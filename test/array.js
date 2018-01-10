d9782ea9b895fa00a526add3aa1d54df3b1174d4
Daniel Gong <daniel.gong@carboncredentials.com>
2018-01-10 10:10:49 +0000

    tests

---
+++ test/array.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+const { toArray } = require('../packages/array/array.js');
+
+describe('Array Module', () => {
+
+    // before(() => {});
+    // beforeEach(() => {});
+
+    describe('toArray', () => {
+
+        it('should convert undefined to an empty array', () => {
+            expect(toArray(undefined)).to.be.an('array').that.is.empty;
+        });
+
+        it('should convert boolean, string, object values to array', () => {
+            expect(toArray(false)).to.deep.equal([ false ]);
+            expect(toArray('')).to.deep.equal([ '' ]);
+            expect(toArray({ a: 1 })).to.deep.equal([ { a: 1 } ]);
+        });
+
+        it('should keep array as is', () => {
+            expect(toArray([1,2,3])).to.deep.equal([1,2,3]);
+        });
+
+    });
+
+});
