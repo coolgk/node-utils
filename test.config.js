4452fd210d34176108ced817d099cf4ea0bf67d3
Daniel Gong <coolgk@gmail.com>
2018-01-11 23:59:12 +0000

    Develop (#1)
    * comment update
    
    * test update

--- test.config.js
+++ test.config.js
@@ -1,11 +1,18 @@
 'use strict';
 
-module.exports = Object.assign(
-    {
-        sourceFolder: 'dist',
-        amqp: {
-            url: ''
-        }
-    },
-    require('./test.config.local.js')
-);
+// module.exports = Object.assign(
+    // {
+        // sourceFolder: 'dist',
+        // amqp: {
+            // url: ''
+        // }
+    // },
+    // require('./test.config.local.js')
+// );
+
+module.exports = {
+    sourceFolder: 'dist',
+    amqp: {
+        url: process.env.AMQP_URL
+    }
+};
