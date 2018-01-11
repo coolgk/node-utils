c6c6412db654f7b038dbc7a07514ca368792b2d4
Daniel Gong <daniel.gong@carboncredentials.com>
2018-01-11 18:03:06 +0000

    test update

--- test/base64.js
+++ test/base64.js
@@ -5,58 +5,33 @@ const expect = require('chai').expect;
 const config = require('../test.config.js');
 
 describe('Base64 Module', () => {
-    /*
-    const { stripTags, prepad0, escapeHtml, unescapeHtml } = require('../dist/string');
 
-    import { encode, decode, encodeUrl, decodeUrl } from '@coolgk/base64';
-    // OR
-    // const { encode, decode, encodeUrl, decodeUrl } = require('@coolgk/base64');
-
-    const a = 'https://www.google.co.uk/?a=b'
-    const hash = encode(a);
-    const urlHash = encodeUrl(a);
-
-    console.log(a); // https://www.google.co.uk/?a=b
-    console.log(hash); // aHR0cHM6Ly93d3cuZ29vZ2xlLmNvLnVrLz9hPWI=
-    console.log(decode(hash)); // https://www.google.co.uk/?a=b
-
-    console.log(urlHash); // aHR0cHM6Ly93d3cuZ29vZ2xlLmNvLnVrLz9hPWI
-    console.log(decodeUrl(urlHash)); // https://www.google.co.uk/?a=b
-
+    const { encode, decode, encodeUrl, decodeUrl } = require(`../${config.sourceFolder}/base64`);
 
     // before(() => {});
     // beforeEach(() => {});
 
-    describe('stripTags', () => {
-        it('should remove html tags', () => {
-            expect(stripTags('<h1>test</h1><script>alert(1)</script>')).to.equal('test alert(1)');
-        });
+    it('should encode', () => {
+        expect(encode('https://www.google.co.uk/?a=b')).to.equal('aHR0cHM6Ly93d3cuZ29vZ2xlLmNvLnVrLz9hPWI=');
     });
 
-    describe('escapeHtml', () => {
-        it('should escape html tags', () => {
-            expect(
-                escapeHtml('<h1>test</h1><script>alert(1)</script>')
-            ).to.equal('&lt;h1&gt;test&lt;/h1&gt;&lt;script&gt;alert(1)&lt;/script&gt;');
-        });
+    it('should decode', () => {
+        expect(decode('aHR0cHM6Ly93d3cuZ29vZ2xlLmNvLnVrLz9hPWI=')).to.equal('https://www.google.co.uk/?a=b');
     });
 
-    describe('unescapeHtml', () => {
-        it('should unescape html tags', () => {
-            expect(
-                unescapeHtml('&lt;h1&gt;test&lt;/h1&gt;&lt;script&gt;alert(1)&lt;/script&gt;')
-            ).to.equal('<h1>test</h1><script>alert(1)</script>');
-        });
+    it('should encode url', () => {
+        expect(encodeUrl('https://www.google.co.uk/?a=b')).to.equal('aHR0cHM6Ly93d3cuZ29vZ2xlLmNvLnVrLz9hPWI');
     });
 
-    describe('prepad0', () => {
-        it('add 0 before a number to a specified length', () => {
-            expect(prepad0(7, 2)).to.equal('07');
-            expect(prepad0(70, 3)).to.equal('070');
-            expect(prepad0(70, 4)).to.equal('0070');
-            expect(prepad0(1, 4)).to.equal('0001');
-            expect(prepad0(1000, 2)).to.equal('1000');
-        });
+    it('should decode url hash', () => {
+        expect(decodeUrl('aHR0cHM6Ly93d3cuZ29vZ2xlLmNvLnVrLz9hPWI')).to.equal('https://www.google.co.uk/?a=b');
     });
-    */
+
+    it('should return empty string for falsy input', () => {
+        expect(encode()).to.equal('');
+        expect(decode()).to.equal('');
+        expect(encodeUrl()).to.equal('');
+        expect(decodeUrl()).to.equal('');
+    });
+
 });
--- test/string.js
+++ test/string.js
@@ -15,6 +15,10 @@ describe('String Module', () => {
         it('should remove html tags', () => {
             expect(stripTags('<h1>test</h1><script>alert(1)</script>')).to.equal('test alert(1)');
         });
+
+        it('should return empty string for falsy input', () => {
+            expect(stripTags()).to.equal('');
+        });
     });
 
     describe('escapeHtml', () => {
@@ -22,6 +26,10 @@ describe('String Module', () => {
             expect(
                 escapeHtml('<h1>test</h1><script>alert(1)</script>')
             ).to.equal('&lt;h1&gt;test&lt;/h1&gt;&lt;script&gt;alert(1)&lt;/script&gt;');
+        });
+
+        it('should return empty string for falsy input', () => {
+            expect(escapeHtml()).to.equal('');
         });
     });
 
@@ -31,10 +39,15 @@ describe('String Module', () => {
                 unescapeHtml('&lt;h1&gt;test&lt;/h1&gt;&lt;script&gt;alert(1)&lt;/script&gt;')
             ).to.equal('<h1>test</h1><script>alert(1)</script>');
         });
+
+        it('should return empty string for falsy input', () => {
+            expect(unescapeHtml()).to.equal('');
+        });
     });
 
     describe('prepad0', () => {
         it('add 0 before a number to a specified length', () => {
+            expect(prepad0(7)).to.equal('07');
             expect(prepad0(7, 2)).to.equal('07');
             expect(prepad0(70, 3)).to.equal('070');
             expect(prepad0(70, 4)).to.equal('0070');
