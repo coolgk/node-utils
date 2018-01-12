481f5f9bf214fd012e03aa4c2d5de3b750e64335
Daniel Gong <daniel.gong@carboncredentials.com>
2018-01-12 18:58:02 +0000

    test queue

---
+++ test/csv.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// const sinon = require('sinon');
+const chai = require('chai');
+chai.use(require("chai-as-promised"));
+const expect = chai.expect;
+
+const config = require('../test.config.js');
+
+describe('CSV Module', function () {
+
+    // const { Captcha } = require(`../${config.sourceFolder}/csv`);
+
+    it('should to do');
+
+});
