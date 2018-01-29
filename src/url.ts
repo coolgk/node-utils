4154029e60a33376bbf412ba43953d8189db20c7
Daniel Gong <daniel.gong@carboncredentials.com>
2018-01-29 15:41:58 +0000

    url version and type udpate

--- src/url.ts
+++ src/url.ts
@@ -1,6 +1,6 @@
 /***
 description: a simple function for parsing parameters in a url
-version: 2.0.4
+version: 2.0.5
 keywords:
     - url
     - parser
@@ -27,7 +27,7 @@ example: |
 */
 
 export interface IParams {
-    [index: string]: string;
+    [index: string]: any;
 }
 
 /**
